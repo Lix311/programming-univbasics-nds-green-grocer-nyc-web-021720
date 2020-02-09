9454402f97cc290cf2bc97a860a37649ba1220c0
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-09 21:54:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -97,6 +97,10 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-
+index = 0 
+  while index < cart.length 
+    cart[index]
+  binding.pry 
+  end 
 
 end
